bc08acda8ff71a06cdbd2a90e086014658ed2a9d
Arnol-Jesus <r.a.p23cruzortiz@gmail.com>
2022-06-23 14:07:36 -0500

    modificacion, add Btons insert, delete, exit and to show

--- Aula Express 2.0/ViewCurso.cs
+++ Aula Express 2.0/ViewCurso.cs
@@ -1,20 +1,136 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Aula_Express_2._0
 {
     public partial class Form6 : Form
     {
+        conexion conMysql = new conexion();
+
         public Form6()
         {
             InitializeComponent();
+        }
+
+        public void limpiar()
+        {
+            textCodigo.Text = " ";
+            textNombre.Text = " ";
+            textDescricion.Text = " ";
+            dataGridView1.ClearSelection();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnguardar_Click(object sender, EventArgs e)
+        {
+            if (textCodigo.Text.Trim() == String.Empty &&
+               textNombre.Text.Trim() == String.Empty &&
+               textDescricion.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("!!!... Error, los campos no pueden estar vacios ...!!!");
+                return;
+            }
+
+            if (textCodigo.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Debes ingresar un Código");
+                return;
+            }
+
+            if (textNombre.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Debes ingresar un Nombre al Curso");
+                return;
+            }
+
+            if (textDescricion.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Debes ingresar una Descripcion");
+                return;
+            }
+
+            String sql = String.Format("INSERT INTO `cursos`(`cod_Curso`, `Nombre`, `Descripcion`) VALUES" + "('"
+                                  + textCodigo.Text + "','" + textNombre.Text + "','" + textDescricion.Text + "')",
+                                   textCodigo.Text.Trim(), textNombre.Text.Trim(), textDescricion.Text.Trim());
+
+            try
+            {
+                if (conMysql.Query(sql) == 1)
+                {
+                    MessageBox.Show("!!!... guardado con éxito ...!!!");
+                }
+                else
+                {
+                    MessageBox.Show("!!!... ERROR, NO se pudo guardar ...!!!");
+                }
+                limpiar();
+                MessageBox.Show(sql);
+            }
+            catch (MySqlException exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void Btnborrar_Click(object sender, EventArgs e)
+        {
+            string sql = String.Format("DELETE FROM `cursos` WHERE `cursos`.`cod_Curso` = '"+ textCodigo.Text +"'", dataGridView1.SelectedRows);
+            if (MessageBox.Show("¿Deseas Eliminar El Curso?", "Eliminar",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    if (conMysql.Query(sql) == 1)
+                    {
+                        MessageBox.Show("!!!... Curso Eliminado ...!!!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("!!!... ERROR, No se pudo eliminar ...!!!");
+                    }
+                    limpiar();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Btnsalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro que deseas salir?", "CURSOS EXPRESS",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                == DialogResult.Yes)
+            {
+                MessageBox.Show("!!!... CERRANDO SESSION ...!!!");
+                this.Close();
+                Application.Exit();
+            }
+        }
+
+        public DataTable ListCursos(string sql)
+        {
+            conMysql.Connect();
+            DataTable ver = new DataTable();
+            conMysql.Query(sql);
+            conMysql.getData(sql);
+            conMysql.getRow(sql);
+            dataGridView1.DataSource = ver;
+            return conMysql.getData(sql);
+        }
+
+        private void Btnmostrar_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM `cursos`";
+            dataGridView1.Select();
+            dataGridView1.DataSource = ListCursos(sql);
         }
     }
 }
